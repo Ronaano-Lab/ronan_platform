4012416e5ffff4d5a1a5766134f58f43914b34c0
Ronan Binny <ronbinny@protonmail.com>
2024-06-28 15:33:21 +1200

    remove extra variables too

--- infra/kubernetes_the_hard_way/variables.tf
+++ infra/kubernetes_the_hard_way/variables.tf
@@ -2,18 +2,10 @@ variable "RESOURCE_GROUP_NAME" {
   type = string
 }
 
-variable "KUBERNETES_VNET_NAME" {
-  type = string
-}
-
 variable "SUBSCRIPTION_ID" {
   type = string
 }
 
 variable "LOCATION" {
-  type = string
-}
-
-variable "STORAGE_ACCOUNT_KEY" {
   type = string
 }
