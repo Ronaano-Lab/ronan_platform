63e78b9d068423d9623fbe15dc8c9d16d9bf5c64
Ronan Binny <ronbinny@protonmail.com>
2024-06-30 18:19:30 +1200

    comment out ssh key

--- infra/kubernetes_the_hard_way/data.tf
+++ infra/kubernetes_the_hard_way/data.tf
@@ -1,4 +1,4 @@
-data "azurerm_ssh_public_key" "SshPublicKey" {
-  name                = var.KUBERNETES_SSH_KEY
-  resource_group_name = azurerm_resource_group.resource_group.name
-}
+# data "azurerm_ssh_public_key" "SshPublicKey" {
+#   name                = var.KUBERNETES_SSH_KEY
+#   resource_group_name = azurerm_resource_group.resource_group.name
+# }
