12c5956cd740e7ef4167b68dff2ae1637d10ea6d
Ronan Binny <ronbinny@protonmail.com>
2024-06-30 16:52:24 +1200

    setup worker vms

--- infra/kubernetes_the_hard_way/main.tf
+++ infra/kubernetes_the_hard_way/main.tf
@@ -59,5 +59,62 @@ resource "azurerm_linux_virtual_machine" "kubernetes_control_plane_vm" {
   location              = var.LOCATION
   resource_group_name   = azurerm_resource_group.resource_group.name
   network_interface_ids = [azurerm_network_interface.kubernetes_control_plane_nic["${count.index}"].id]
-  admin_username        = "kube-root"
+  admin_username        = "kuberoot"
+}
+
+
+resource "azurerm_public_ip" "kubernetes_worker_pip" {
+  count               = 3
+  name                = "pip-kubernetesworker-dev-aue-${count.index}"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.LOCATION
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "kubernetes_worker_nic" {
+  count = 3
+  name  = "worker-${count.index}-nic"
+
+  ip_configuration {
+    name                          = "internal-ip-${count.index}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.240.0.1${count.index}"
+    subnet_id                     = azurerm_subnet.kubernetes_subnet.id
+    public_ip_address_id          = azurerm_public_ip.kubernetes_worker_pip["${count.index}"].id
+  }
+  enable_ip_forwarding = true
+  location             = var.LOCATION
+  resource_group_name  = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_linux_virtual_machine" "kubernetes_worker_vm" {
+  count = 3
+  name  = "vm-kubernetesworker-dev-aue-${count.index}"
+  size  = "Standard_B1ls"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  availability_set_id = azurerm_availability_set.availability_set.id
+
+  admin_ssh_key {
+    username   = "kube-root"
+    public_key = data.azurerm_ssh_public_key.SshPublicKey.public_key
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+  location              = var.LOCATION
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  network_interface_ids = [azurerm_network_interface.kubernetes_worker_nic["${count.index}"].id]
+  admin_username        = "kuberoot"
+  tags = {
+    "pod-cidr" = "10.200.${count.index}.0/24"
+  }
 }
