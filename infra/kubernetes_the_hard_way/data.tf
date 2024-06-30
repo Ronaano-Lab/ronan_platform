3065ff0ad7860f3c6476d004071d19672b7f864d
Ronan Binny <ronbinny@protonmail.com>
2024-06-30 15:39:24 +1200

    fix resource group name reference

--- infra/kubernetes_the_hard_way/data.tf
+++ infra/kubernetes_the_hard_way/data.tf
@@ -1,4 +1,4 @@
 data "azurerm_ssh_public_key" "SshPublicKey" {
   name                = var.KUBERNETES_SSH_KEY
-  resource_group_name = VAR.RESOURCE_GROUP_NAME
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
